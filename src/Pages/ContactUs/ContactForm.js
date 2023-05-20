e283fcfa8b1ae4ab92d6cc1643620a174f0f1ab4
Noor Aldeen <anooraldeen9@gmail.com>
2023-05-20 22:57:59 +0300

    finish functionality of contact us

--- src/Pages/ContactUs/ContactForm.js
+++ src/Pages/ContactUs/ContactForm.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Form, Col, Row, Button } from "react-bootstrap";
 import { BsArrowRight } from "react-icons/bs";
 import Style from "./Style";
 import { useTheme } from "react-jss";
+import sendEmail from './SendEmail';
 const ContactForm = () => {
   const theme = useTheme();
   const classes = Style(theme);
+  const [email, setEmail] = useState({name:'',email:'',message:''});
+  const [response, setResponse] = useState(true)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEmail((prevState) => ({
+      ...prevState,
+      [name]: value,}))
+    }
+  const handleSend=async()=>{
+    const res = await sendEmail(email.name,email.email,email.message,"s11923513@stu.najah.edu")
+    setResponse(res)
+    if(res) window.alert('email send successfully')
+
+  }
+
   return (
     <Form className={`${classes.Form} `}>
     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
       <Form.Label>Your Name</Form.Label>
-      <Form.Control type="text" placeholder="Write your name" />
+      <Form.Control type="text" placeholder="Write your name" name='name' onChange={(e)=>{handleChange(e)}}/>
     </Form.Group>
     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
       <Form.Label>Your Email</Form.Label>
-      <Form.Control type="email" placeholder="Sample@gmail.com" />
+      <Form.Control type="email" placeholder="Sample@gmail.com" name='email' onChange={(e)=>{handleChange(e)}}/>
     </Form.Group>
     <Form.Group
       className="mb-3"
@@ -25,16 +41,20 @@ const ContactForm = () => {
         as="textarea"
         rows={6}
         placeholder="Write here..........."
+        name='message'
+        onChange={(e)=>{handleChange(e)}}
         style={{ resize: 'none' }}
       />
     </Form.Group>
     <Form.Group as={Row} className="mb-3">
       <Col className='d-flex flex-column align-items-end'>
-        <Button>
-          Send <BsArrowRight />{" "}
+        <Button onClick={handleSend}>
+          Send <BsArrowRight />
         </Button>
       </Col>
     </Form.Group>
+    {!response && setResponse(true)}
+    {!response && window.alert('your email wrong') }
   </Form>
   )
 }
---
+++ src/Pages/ContactUs/SendEmail.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const sendEmail = async(senderName, senderEmail, message, recipientEmail)=> {
+  const apiKey = '95f78dd5b72febf7c53cc62fd8134704-db4df449-c6946407';
+  const apiUrl = 'https://api.mailgun.net/v3/sandbox5ee3242cf1ce4bdb86ca5b6a8afd0491.mailgun.org/messages';
+  const data = new FormData();
+  data.append('from', `${senderName} <${senderEmail}>`);
+  data.append('to', `Noor aldeen abuShehadeh <${recipientEmail}>`);
+  data.append('subject', 'Hello CodeScape I want Service');
+  data.append('text', message);
+  
+  return axios.post(apiUrl, data, {
+    auth: {
+      username: 'api',
+      password: apiKey
+    },
+  })
+    .then(response => {
+      console.log('Email sent successfully!');
+      console.log('Response:', response.data);
+      return true
+    })
+    .catch(error => {
+      console.error('Error occurred:', error.response.data);
+      return false
+    });
+}
+
+export default sendEmail
